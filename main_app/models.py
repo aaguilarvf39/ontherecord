d332ee344f5aac2610ad02843918bf18603ee8c4
username <youremail@domain.com>
2022-08-15 10:08:16 -0700

    made first model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+RATING = (
+    ('1', '1 Star'),
+    ('2', '2 Star'),
+    ('3', '3 Star'),
+    ('4', '4 Star'),
+    ('5', '5 Star'),
+)
+
 
 # Create your models here.
+class Locator(models.Model):
+    location = models.CharField(max_length=500)
+    hours = models.IntegerField()
+    reviews = models.TextField(max_length=1000)
+    rating = models.CharField(
+        max_length=1,
+        choices=RATING,
+        default=RATING[4][0]
+    )
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
@@ -23,3 +24,4 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
