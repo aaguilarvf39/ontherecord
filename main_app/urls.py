5d8328987e1785f9db4b819557263a5b86e1cc5a
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-08-18 16:47:39 -0700

    working on update and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('locator/', views.locator, name='locator'),
     path('locator/<int:shop_id>/', views.locator_detail, name='detail'),
     path('locator/<int:shop_id>/add_review/', views.add_review, name='add_review'),
+    path('locator/<int:pk>/update_review/', views.ReviewUpdate.as_view(), name='update_review'),
+    # path('locator/<int:pk>/delete_review/', views.ReviewDelete.as_view(), name='delete_review'),
 ]
+
