bc24fd475f43f7f94e17ea87d74cfdbda99be393
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-08-18 15:39:35 -0700

    Reviews rending on detail html

--- main_app/models.py
+++ main_app/models.py
@@ -4,14 +4,6 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-RATING = (
-    ('1', '1 Star'),
-    ('2', '2 Star'),
-    ('3', '3 Star'),
-    ('4', '4 Star'),
-    ('5', '5 Star'),
-)
-
 
 # Create your models here.
 class Locator(models.Model):
@@ -31,12 +23,9 @@ class Locator(models.Model):
 
 
 class Review(models.Model):
-    reviews = models.TextField(max_length=1000)
-    rating = models.CharField(
-        max_length=1,
-        choices=RATING,
-        default=RATING[4][0]
-    )
+    review = models.TextField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    locator = models.ForeignKey(Locator, on_delete=models.CASCADE)
 
 
 class UserProfile(models.Model):
